5b450f081016330e5e55a474e543992b422ff88e
Yusra Fatima <162156870+Usrafatima@users.noreply.github.com>
2024-11-10 03:17:06 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
       <h1 className='itberries'>IT Berries</h1>
       <p className='it-para'>Yusra is a front-end developer passionate about creating engaging and user-friendly web experiences. 
         With a focus on clean design and intuitive interfaces, she blends technical skills with creativity to deliver effective solutions. 
-        Yusra is currently pursuing a Bachelor's degree in Statistics with Finance, which enhances her analytical thinking. 
+        Yusra is currently pursuing a Bachelors degree in Statistics with Finance, which enhances her analytical thinking. 
         </p>
         
        <button  className='read-btn'>Read more</button>
